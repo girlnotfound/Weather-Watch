d14066196be5ab5f4df03c2304fa7d8839816ff9
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-05-08 13:19:13 -0700

    Added div to html to host the populated search history. Added media queries to css to make the site responsive. Added variables to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+// create variables for API key and base URL
 const apiKey = '348408785b0788ac19a93045becfa9a2'; 
 const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}';
+let city = "";
+// create variable to retreive history from local storage
+let searchHistory = JSON.parse(localStorage.getItem("search-history")) || [];
+// create variables to reference html elements 
 const formEl = document.getElementById('form');
 const searchBar = document.getElementById('searchbar');
 const dashboardEl = document.getElementById('dashboard');
 const cardEl = document.getElementsByClassName('weather-card');
+const submitBtn = document.querySelector('#submit-button');
