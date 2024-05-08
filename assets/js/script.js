5b8c5d28a79167874598175dbb45033d756b417e
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-05-07 19:38:28 -0700

    Added variables and API Key/URL to js file

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const apiKey = '348408785b0788ac19a93045becfa9a2'; 
+const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}';
+const formEl = document.getElementById('form');
+const searchBar = document.getElementById('searchbar');
+const dashboardEl = document.getElementById('dashboard');
+const cardEl = document.getElementsByClassName('weather-card');
