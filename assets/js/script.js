1ad8991744f55242f7dc5b04d37185db9eb1639b
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-05-09 12:52:45 -0700

    Removed variable 'apiUrl' also  added functions to get city coordinates, and get weather data to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 // create variables for API key and base URL
 const apiKey = '348408785b0788ac19a93045becfa9a2'; 
-const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}';
 let city = "";
-// create variable to retreive history from local storage
+// create variable to retreive search history from local storage
 let searchHistory = JSON.parse(localStorage.getItem("search-history")) || [];
 // create variables to reference html elements 
 const formEl = document.getElementById('form');
@@ -10,3 +9,44 @@ const searchBar = document.getElementById('searchbar');
 const dashboardEl = document.getElementById('dashboard');
 const cardEl = document.getElementsByClassName('weather-card');
 const submitBtn = document.querySelector('#submit-button');
+const searchHistoryEl = document.getElementById('search-history');
+
+// Function to get city coordinates 
+function getCityCoordinates(city, apiKey) {
+    fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=1&appid=${apiKey}`
+    )
+        .then(function (response) {
+            return response.json(); // convert to JSON
+        })
+        .then(function (data) {
+             // check if no data was returned
+            if (data.length === 0) {
+                alert("Location not found, Please try searching for a city.");
+            } else {
+                // extract lattitude and longitude
+                let lat = data[0].lat; 
+                let lon = data[0].lon; 
+                getWeatherData(lat, lon, apiKey); // call the weather API function to get the weather data
+            }
+        });
+}
+
+// function to get weather data
+function getWeatherData(lat, lon, apiKey) {
+    fetch('http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}'
+
+    )
+        .then(function(response) {
+            return response.json(); // convert to JSON
+        })
+        .then(function(data) {
+             // check if no weather data is available
+            if (data.length === 0) {
+                alert("Sorry, currently no weather information available.");
+            } else {
+                // log the forcast weather data to the console
+                console.log("Forecast Data:", data);
+            }
+        });
+}
